868f55e35e734becc7000b65bce3783d33c441a9
Morgan Campbell <jmorgancampbell@gmail.com>
2014-09-22 18:16:25 -0500

    Live data, new map

--- index.php
+++ index.php
@@ -13,15 +13,16 @@
         <div id='map'></div>
 
         <script>
-            var map = L.mapbox.map('map', 'obrit.j3l6pe74')
-                .setView([33.22, -97.12], 13);
+            var map = L.mapbox.map('map', 'obrit.f5bd7c3c')
+                .setView([33.2191, -97.1373], 12);
 
             <?php
-            $json_string = file_get_contents("weight_log.json");
-            $json_string = str_replace('ISODate', 'Date', $json_string);
-            $json_string = preg_replace('/(ObjectId\()("[0-9A-Za-z]*")(\))/', '$2', $json_string);
+            $json_string = file_get_contents("https://compostdenton.com/weight");
+            //$json_string = str_replace('ISODate', 'Date', $json_string);
+            //$json_string = preg_replace('/(ObjectId\()("[0-9A-Za-z]*")(\))/', '$2', $json_string);
             ?>
-            var json_data = <?php echo str_replace(array("\n", "\t", " "), '', $json_string); ?>;
+            //var json_data = <?php //echo str_replace(array("\n", "\t", " "), '', $json_string); ?>;
+            var json_data = <?php echo $json_string; ?>;
 
             var geoJsonData = {
                 type: "FeatureCollection",
@@ -49,10 +50,32 @@
                         // Here we use the `count` property in GeoJSON verbatim: if it's
                         // to small or two large, we can use basic math in Javascript to
                         // adjust it so that it fits the map better.
-                        radius: feature.properties.count
+                        //radius: feature.properties.count
                     })
                 }
             }).addTo(map);
+
+
+            // TEST GRIDLAYER STUFF
+            // L.mapbox.accessToken = 'pk.eyJ1Ijoib2JyaXQiLCJhIjoiMlRUQ0hxQSJ9.--yh59XkySo_ce2g6B7b3g';
+            // // Define a map without a Map ID so we
+            // // have to add each part of it manually.
+            // var map = L.mapbox.map('map', undefined, {
+            //     infoControl: true,
+            //     attributionControl: false
+            // });
+
+            // // The visible tile layer
+            // L.mapbox.tileLayer('obrit.f5bd7c3c').addTo(map);
+
+            // // Load interactivity data into the map with a gridLayer
+            // var myGridLayer = L.mapbox.gridLayer('obrit.f5bd7c3c').addTo(map);
+
+            // // And use that interactivity to drive a control the user can see.
+            // var myGridControl = L.mapbox.gridControl(myGridLayer).addTo(map);
+
+            // // Finally, center the map.
+            // map.setView([33.2191, -97.1373], 13);
         </script>
     </body>
 </html>
